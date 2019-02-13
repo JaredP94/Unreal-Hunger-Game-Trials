d18f2734a4cc921e85ac4c7d9e7317dd9a60e59d
Jared <jaredping@yahoo.com>
2019-02-13 21:06:17 +0200

    Setup and access blackboard index value

--- HungerGameTrials/Source/HungerGameTrials/ChooseNextWaypoint.cpp
+++ HungerGameTrials/Source/HungerGameTrials/ChooseNextWaypoint.cpp
@@ -1,9 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("C++ BP Running"));
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	return EBTNodeResult::Succeeded;
 }
--- HungerGameTrials/Source/HungerGameTrials/ChooseNextWaypoint.h
+++ HungerGameTrials/Source/HungerGameTrials/ChooseNextWaypoint.h
@@ -16,4 +16,7 @@ class HUNGERGAMETRIALS_API UChooseNextWaypoint : public UBTTaskNode
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+		struct FBlackboardKeySelector IndexKey;
 };

187a0230289927a212255e228fb6eeb121356e72
Jared <jaredping@yahoo.com>
2019-02-17 11:08:16 +0200

    Remove deprecated class

--- HungerGameTrials/Source/HungerGameTrials/PatrollingGuard.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PatrollingGuard.h"
-
-TArray<AActor*> APatrollingGuard::GetPatrolPoints()
-{
-	return PatrolPointsCPP;
-}
